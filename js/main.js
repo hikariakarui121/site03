468233ae84093b1171b8b439a733150cd80ec36c
hikariakarui121 <80142147+hikariakarui121@users.noreply.github.com>
2021-06-15 15:10:35 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,3 +4,21 @@ $(function(){
       $('#nav').toggleClass("-active");
   })
 });
+
+// スクロール
+$('a[href^="#"]').click(function(){
+  var speed = 500;
+  var href= $(this).attr("href");
+  var target = $(href == "#" || href == "" ? 'html' : href);
+  var position = target.offset().top -60;
+  $("html, body").animate({scrollTop:position}, speed, "swing");
+  return false;
+});
+
+
+// aosについて
+AOS.init({
+  easing: 'ease-in',
+  duration: 800,
+  once: true,
+})
